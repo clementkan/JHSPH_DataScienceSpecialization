0a63964d7e44a009fd8e716ea5d76f27c2bbffbe
Clement Kan <clement_kan@hotmail.com>
2015-10-20 15:34:56 +0800

    Update complete.R

--- complete.R
+++ complete.R
@@ -2,20 +2,39 @@ Solution 1 of 3:
 complete <- function(directory, id = 1:332) {
         files<-list.files(directory,full.names=T)
         
-        dat<-data.frame()
         for (i in id){
                 x<-read.csv(files[i])
                 good<-complete.cases(x)
                 y<-nrow(x[good,])
                 z<-cbind(i,y)
-                
-                dat<-rbind(dat, z)
         }
+        
+        dat<-data.frame()
+        dat<-rbind(dat, z)
         names(dat)<-c("id",'nobs')
         print(dat)
 }
 
 Solution 2 of 3:
+complete <- function(directory, id = 1:332){
+        files <- list.files(directory, full.names = TRUE)
+        
+        dat <- data.frame()
+        nobs <- data.frame()
+        
+        for(i in id){
+                dat <- read.csv(files[i])
+                good <- complete.cases(dat)
+                x <- dat[good,]
+                nobs <- rbind(nobs, nrow(x))
+        }
+        
+        y <- cbind(id, nobs)
+        colnames(y)<-c("id","nobs")
+        print(y)
+}
+
+Solution 3 of 3:
 complete <- function(directory, id = 1:332){
         files_list <- list.files(directory, full.names = TRUE)
         dat <- data.frame()
@@ -28,21 +47,5 @@ complete <- function(directory, id = 1:332){
         colnames(x)<-c("id","nobs")
         target <- id
         y <- x[match(target,x$id),]
-        print(y)
-}
-
-Solution 3 of 3:
-complete <- function(directory, id = 1:332){
-        files_list <- list.files(directory, full.names = TRUE)
-        files_read <- data.frame()
-        nobs <- data.frame()
-        for(i in id){
-                files_read <- read.csv(files_list[i])
-                good <- complete.cases(files_read)
-                x <- files_read[good,]
-                nobs <- rbind(nobs, nrow(x))
-                }
-        y <- cbind(id, nobs)
-        colnames(y)<-c("id","nobs")
         print(y)
 }
