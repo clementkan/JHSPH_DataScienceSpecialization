25accbdd367adb51a14d2a69e6c807ce98c407c5
Clement Kan <clement_kan@hotmail.com>
2015-10-20 14:15:11 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,17 @@
+Solution 1 of 2:
+pollutantmean <- function(directory, pollutant, id = 1:332){
+        files<-list.files(directory, full.names=T)
+        
+        dat<-data.frame()
+        for(i in id){
+                dat<-rbind(dat, read.csv(files[i]))
+        }
+        
+        subset<- dat[,pollutant]
+        mean(subset, na.rm=T)
+}
+
+Solution 2 of 2:
 pollutantmean <-function(directory, pollutant, id = 1:332){
         files_list <- list.files(directory, full.names = TRUE)
         dat <- data.frame()
