70d2a1dad3d5f4d70ddf9399be4dcca3ab2665fb
Clement Kan <clement_kan@hotmail.com>
2015-10-21 19:08:02 +0800

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,16 +1,19 @@
-corr <- function(directory, threshold = 0){
-        files_list <- list.files(directory, full.names = TRUE)
-        dat <- vector()
+Solution 1:
+corr <- function(directory, threshold = 0) {
+        files<-list.files(directory, full.names=T)
+        
+        dat<-vector()
         for(i in 1:332){
-                read_file <- read.csv(files_list[i])
-                good <- complete.cases(read_file)
-                y <- read_file[good,]
-                nob <- nrow(y)
-                if(nob > threshold){
-                        x <- cor(read_file$nitrate, read_file$sulfate, use ="complete.obs")
-                        dat <- rbind(dat, x)
+                x<-read.csv(files[i])
+                good<-complete.cases(x)
+                y<-x[good,]
+                z<-nrow(y)
+                
+                if(z > threshold){
+                        c <- cor(sulfate, nitrate, use ="complete.obs")
+                        dat <- c(dat, c)
                 }
+                
         }
-        z <- as.numeric(dat)
-        print(z)
+        print(z)        
 }
