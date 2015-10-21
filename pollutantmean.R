36eba4feb836c26d836987d30831553d5cc6e7d5
Clement Kan <clement_kan@hotmail.com>
2015-10-22 00:08:18 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,27 +1,30 @@
-Solution 1 of 2:
+## Solution 1 of 2:
 pollutantmean <- function(directory, pollutant, id = 1:332) {
         files<-list.files(directory, full.names=TRUE)
-        
+                
         data<-data.frame()
         for(i in id){
                 data<-rbind(data, read.csv(files[i]))
         }
-        
+                
         pollutantmean<-data[,pollutant]
         mean(pollutantmean, na.rm=TRUE)
 }
 
-Solution 2 of 2:
-pollutantmean <-function(directory, pollutant, id = 1:332){
+## Solution 2 of 2:
+pollutantmean <-function(directory, pollutant, id = 1:332) {
         files_list <- list.files(directory, full.names = TRUE)
+        
         dat <- data.frame()
         for(i in id){
                 dat <- rbind(dat, read.csv(files_list[i]))
         }
+        
         if(pollutant == "sulfate"){
                 dat_subset <- dat[,"sulfate"]
-        } else{
+        } else {
                 dat_subset <- dat[,"nitrate"]
         }
+        
         mean(dat_subset, na.rm = TRUE)
 }
