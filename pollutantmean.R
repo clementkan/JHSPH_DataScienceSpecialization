91c8ec9e95936aca3934d02f3cc798c55b038220
Clement Kan <clement_kan@hotmail.com>
2016-02-24 17:23:25 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,30 +1,29 @@
 ## Solution 1 of 2:
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        files<-list.files(directory, full.names=TRUE)
+        files <- list.files(directory, full.names=TRUE)
                 
-        data<-data.frame()
+        data <- data.frame()
         for(i in id){
-                data<-rbind(data, read.csv(files[i]))
+                data <- rbind(data, read.csv(files[i]))
         }
-                
-        pollutantmean<-data[,pollutant]
-        mean(pollutantmean, na.rm=TRUE)
+        
+        mean(data[,pollutant], na.rm=TRUE)
 }
 
 ## Solution 2 of 2:
-pollutantmean <-function(directory, pollutant, id = 1:332) {
-        files_list <- list.files(directory, full.names = TRUE)
-        
-        dat <- data.frame()
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        files <- list.files(directory, full.names=TRUE)
+                
+        data <- data.frame()
         for(i in id){
-                dat <- rbind(dat, read.csv(files_list[i]))
+                data <- rbind(data, read.csv(files[i]))
         }
         
         if(pollutant == "sulfate"){
-                dat_subset <- dat[,"sulfate"]
+                subset <- data[,"sulfate"]
         } else {
-                dat_subset <- dat[,"nitrate"]
+                subset <- data[,"nitrate"]
         }
         
-        mean(dat_subset, na.rm = TRUE)
+        mean(subset, na.rm = TRUE)
 }
