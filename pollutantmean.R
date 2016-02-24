4440f56955721f6d3fa2e79a31fd8a5c248e52c9
Clement Kan <clement_kan@hotmail.com>
2016-02-24 17:58:22 +0800

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,16 +1,22 @@
 ## Solution 1 of 2:
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-        files <- list.files(directory, full.names=TRUE)
-                
-        data <- data.frame()
+## Fastest solution, create an empty list (tmp) thats the length of the expected output.
+## Refer to R programming, week 2, practice assignment for more information. 
+pollutantmean <- function(directory, pollutant, id = 1:332){
+        files <- list.files(directory, full.names = TRUE)
+        
+        tmp <- vector(mode = "list", length = length(id))
+        
         for(i in id){
-                data <- rbind(data, read.csv(files[i]))
+                tmp[[i]] <- read.csv(files[[i]])
         }
+        
+        data <- do.call(rbind, tmp) ## do.call() to combine tmp into a single data frame.
         
         mean(data[,pollutant], na.rm=TRUE)
 }
 
 ## Solution 2 of 2:
+## Slower as compare to solution 1, the data frame is constantly being recopy at each loop. 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
         files <- list.files(directory, full.names=TRUE)
                 
