2675cfd76417374b16c4d5786edfc0b355882742
Clement Kan <clement_kan@hotmail.com>
2016-10-27 11:35:50 +0800

    Update complete.R

--- R_Programming/complete.R
+++ R_Programming/complete.R
@@ -1,40 +1,22 @@
 ## R Programming (week 2): Assignment 1, Air Pollution
-## Solution 1 of 3:
-complete <- function(directory, id = 1:332){
+## Solution 1 of 2:
+complete <- function(directory, id = 1:332) {
         files <- list.files(directory, full.names = TRUE)
         
         data <- data.frame()
         for(i in id){
                 x <- read.csv(files[i])
                 good <- complete.cases(x)
-                nobs <- c(i, nrow(x[good,]))
-                data <- rbind(data, nobs)
+                
+                y <- c(i, nrow(x[good,]))
+                
+                data <- rbind(data, y)
         }
-        
-        names(data) <- c("id", "nobs")
+        colnames(data) <- c("id", "nobs")
         return(data)
 }
-## Solution 2 of 3:
-## Works, but not a good solution.
-complete <- function(directory, id = 1:332){
-        files <- list.files(directory, full.names = TRUE)
-        
-        dat <- data.frame()
-        nobs <- data.frame()
-        
-        for(i in id){
-                dat <- read.csv(files[i])
-                good <- complete.cases(dat)
-                x <- dat[good,]
-                nobs <- rbind(nobs, nrow(x))
-        }
-        
-        y <- cbind(id, nobs)
-        names(y) <- c("id","nobs")
-        print(y)
-}
 
-## Solution 3 of 3:
+## Solution 2 of 2:
 ## Solution from other people.
 complete <- function(directory, id = 1:332){
         files_list <- list.files(directory, full.names = TRUE)
