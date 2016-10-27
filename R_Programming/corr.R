03881ab0e1c3855053ce64fac5b90a82f051556f
Clement Kan <clement_kan@hotmail.com>
2016-10-27 15:25:47 +0800

    Update corr.R

--- R_Programming/corr.R
+++ R_Programming/corr.R
@@ -1,9 +1,9 @@
 ## R Programming (week 2): Assignment 1, Air Pollution
 ## Solution 1 of 1:
-## This script checks if the number of completed cases > threshold given,
+## This script checks if the number of complete cases > threshold given,
 ## if "yes", its calculates the correlation between sulfate and nitrate for the monitor.
 ## Returns a numeric vector containing the correlations.
-corr <- function(directory, threshold = 0){
+corr <- function(directory, threshold = 0) {
         files <- list.files(directory, full.names = TRUE)
         
         data <- numeric()
@@ -11,11 +11,12 @@ corr <- function(directory, threshold = 0){
                 x <- read.csv(files[i])
                 good <- complete.cases(x)
                 y <- x[good,]
-                no.cases <- nrow(y)
-                if(no.cases > threshold){
-                        cr <- cor(y$sulfate, y$nitrate) ## alternative: cr <- cor(x$sulfate, x$nitrate, use = "complete.obs") 
+                complete_cases <- nrow(y)
+             
+                if(complete_cases > threshold){
+                        cr <- cor(y$sulfate, y$nitrate)
                         data <- c(data, cr)
-                }
+                }   
         }
         return(data)
 }
