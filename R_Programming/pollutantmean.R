3ba1cade24db9778cb0707db0c6ac8b62f6c31d4
Clement Kan <clement_kan@hotmail.com>
2016-12-12 13:28:57 +0800

    Update pollutantmean.R

--- R_Programming/pollutantmean.R
+++ R_Programming/pollutantmean.R
@@ -1,11 +1,19 @@
-## R Programming (week 2): Assignment 1, Air Pollution
+## R Programming Assignment 1 Answers - week 2: Air Pollution
+
+## Question 1: pollutantmean.R
+## Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in the 'directory' argument and returns the mean of the pollutant across all of the monitors, ignoring any missing values coded as NA.
+
+pollutantmean <- function(directory, pollutant, id = 1:332)
+
 ## Solution 1 of 2:
-## Fastest solution, create an empty list (tmp) thats the length of the expected output.
-## Refer to R programming, week 2, practice assignment for more information. 
+## Fastest solution by create an empty list (tmp) that’s the length of the expected output.
+## Refer to R programming practice assignment - week 2 for more information. 
+
 pollutantmean <- function(directory, pollutant, id = 1:332){
         files <- list.files(directory, full.names = TRUE)
         
         tmp <- vector(mode = "list", length = length(id))
+        
         for(i in id){
                 tmp[[i]] <- read.csv(files[[i]])
         }
@@ -15,15 +23,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 }
 
 ## Solution 2 of 2:
-## Slower as compare to solution 1, the data frame is recopied at each loop. 
+## Slower than solution 1 because the data frame is recopied at each loop. 
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
         files <- list.files(directory, full.names = TRUE)
+        
         data <- data.frame()
         
         for(i in id){
                 data <- rbind(data, read.csv(files[i]))
         }
-
+        
         subset <- data[,pollutant]
         mean(subset, na.rm = TRUE)
 }
