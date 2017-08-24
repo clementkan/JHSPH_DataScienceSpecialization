5596fb2a36a18dccacbdc6ce27fce01808feb822
Clement Kan <clement_kan@hotmail.com>
2017-08-24 13:41:14 +0800

    Update best.R

--- R_Programming/Assignment 3/best.R
+++ R_Programming/Assignment 3/best.R
@@ -1,8 +1,10 @@
 best <- function(state, outcome){
+        
         ## Read outcome data
         data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        data2 <- data1[,c(7,2,11,17,23)]
-        colnames(data2) <- c("State", "hospital name","heart attack", "heart failure", "pneumonia")
+        
+        data2 <- data1[, c(2, 7, 11, 17, 23)]
+        names(data2) <- c("Hospital.name", "State", "heart attack", "heart failure", "pneumonia")
         
         ## Check that state and outcome are valid
         if(!state %in% data2$State){
@@ -13,10 +15,13 @@ best <- function(state, outcome){
         }
         
         ## Return hospital name in that state with lowest 30-day death rate
-        data3 <- data2[data2[,"State"] == state, c("hospital name", outcome)]
-        data3[,2] <- as.numeric(data3[,2])
+        data3 <- data2[data2$State == state, c("Hospital.name", outcome)]
         
-        x <- data3[order(data3[,2], data3[,1]),]
-        z <- x[1,1]
-        return(z)
+        data3[data3[,2] == "Not Available",] <- NA
+        data4 <- na.omit(data3)
+        data4[,2] <- as.numeric(data4[,2])
+        
+        data5 <- data4[order(data4[,2], data4[,1]),]
+        return(data5[1,1])
+        
 }
