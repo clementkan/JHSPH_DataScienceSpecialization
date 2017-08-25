ba31c1a703f3bbc93dd59001b8a44fb16f60048b
Clement Kan <clement_kan@hotmail.com>
2017-08-25 10:28:24 +0800

    Update rankall.R

--- R_Programming/Assignment 3/rankall.R
+++ R_Programming/Assignment 3/rankall.R
@@ -1,32 +1,38 @@
+# Solution 1 of 2:
+# This solution is easier for beginner to understand the code. The approach for building the 
+# data frame is suboptimal, its build the dataframe by coping and re-copying them inside of 
+# the loop. Solution 2 is a better approach to create a dataframe.
 rankall <- function(outcome, num = "best"){
-        ##Read outcome data
+        
+        ## Read outcome data
         data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         
-        ##Check that outcome is valid
-        if(!outcome %in% c("heart attack", "heart failure", "pneumonia")){
-                stop("invalid outcome")
-        }
-        
-        ##For each state, find the hospital of the given rank
-        switch(outcome, 
+        switch(outcome,
                "heart attack" = {col <- 11},
                "heart failure" = {col <- 17},
                "pneumonia" = {col <- 23}
         )
         
-        data2 <- data1[,c(2, 7, col)]
-        ## colnames(data2) <- c("hospital name", "state", outcome)
+        data2 <- data1[, c(2, 7, col)]
+        names(data2) <- c("hospital", "state", outcome)
         
+        ## Check that outcome is valid
+        if(!outcome == names(data2[3])){
+                stop("invalid outcome")
+        }
+        
+        ## Ranking hospitals in all states
         data2[data2[,3] == "Not Available",] <- NA
         data3 <- na.omit(data2)
         data3[,3] <- as.numeric(data3[,3])
-        data3 <- data3[order(data3[,2], data3[,3], data3[,1]),]
+        data3 <-data3[order(data3[,2], data3[,3], data3[,1]),] #order by state, outcome, hospital
         
-        unique_state <- unique(data3[,2])
+        unique_state <- unique(data3$state)
+        
         output <- data.frame()
         
         for(i in seq(unique_state)){
-                x <- data3[data3[,2] == unique_state[i],]
+                x <- data3[data3$state == unique_state[i],]
                 
                 if(num == "best"){result <- x[1,1]}
                 else if(num == "worst"){result <- x[nrow(x),1]}
@@ -35,11 +41,60 @@ rankall <- function(outcome, num = "best"){
                 
                 y <- cbind(result, unique_state[i])
                 output <- rbind(output, y)
-                
         }
         
-        output <- output[order(output[,2]),]
-        rownames(output) <- output[,2]
-        colnames(output) <- c("hospital", "state")
-        data.frame(output)
+        names(output) <- c("hospital","state")
+        row.names(output) <- unique_state
+        
+        return(output)
+}
+
+# Solution 2 of 2:
+# This solution is a better approach to create a dataframe.
+rankall <- function(outcome, num = "best"){
+        
+        ## Read outcome data
+        data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        switch(outcome, 
+               "heart attack" = {col <- 11},
+               "heart failure" = {col <- 17},
+               "pneumonia" = {col <- 23}
+        )
+        
+        data2 <- data1[, c(2, 7, col)]
+        names(data2) <- c("hospital", "state", outcome)
+        
+        ## Check that outcome is valid
+        if(!outcome == names(data2[3])){
+                stop("invalid outcome")
+        }
+        
+        ## Ranking hospitals in all states
+        data2[data2[,3] == "Not Available",] <- NA
+        data3 <- na.omit(data2)
+        data3[,3] <- as.numeric(data3[,3])
+        data3 <-data3[order(data3[,2], data3[,3], data3[,1]),] #order by state, outcome, hospital
+        
+        unique_state <- unique(data3$state)
+        
+        tmp <- vector(mode = "list", length = length(unique_state))
+        
+        for(i in seq(unique_state)){
+                x <- data3[data3$state == unique_state[i],]
+                
+                if(num == "best"){result <- x[1,1]}
+                else if(num == "worst"){result <- x[nrow(x),1]}
+                else if(num %in% 1:nrow(x)){result <- x[num,1]}
+                else if(num > nrow(x)){result <- NA}
+                
+                tmp[[i]] <- c(result, unique_state[i])
+        }
+        output <- do.call(rbind,tmp)
+        output <- data.frame(output) #convert ouput from matrix to data.frame
+        
+        names(output) <- c("hospital","state")
+        row.names(output) <- unique_state
+        
+        return(output)
 }
