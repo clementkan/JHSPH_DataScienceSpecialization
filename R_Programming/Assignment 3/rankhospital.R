a9f1c2695f8f5f602b88da7c101dfaa73013f879
Clement Kan <clement_kan@hotmail.com>
2017-08-25 10:29:18 +0800

    Update rankhospital.R

--- R_Programming/Assignment 3/rankhospital.R
+++ R_Programming/Assignment 3/rankhospital.R
@@ -1,10 +1,18 @@
 rankhospital <- function(state, outcome, num = "best"){
+        
         ## Read outcome data
         data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        data2 <- data1[,c(7,2,11,17,23)]
-        colnames(data2) <- c("State", "hospital name","heart attack", "heart failure", "pneumonia")
         
-        ## Check that state and outcome are valid  
+        switch(outcome, 
+               "heart attack" = {col <- 11},
+               "heart failure" = {col <- 17},
+               "pneumonia" = {col <- 23}
+        )
+        
+        data2 <- data1[, c(2, 7, col)]
+        names(data2) <- c("Hospital.name", "State", outcome)
+        
+        ## Check that state and outcome are valid
         if(!state %in% data2$State){
                 stop("invalid state")
         }
@@ -12,23 +20,22 @@ rankhospital <- function(state, outcome, num = "best"){
                 stop("invalid outcome")
         }
         
-        ## Return hospital name in that state with the given rank 30-day death rate
-        data3 <- data2[data2[,"State"] == state, c("hospital name", outcome)]
-        data3[,2] <- as.numeric(data3[,2])
-        data3 <- data3[order(data3[,2],data3[,1]),]
+        ## Ranking hospitals by outcome in a state
+        data3 <- data2[data2$State == state, c("Hospital.name", outcome)]
         
-        good <- complete.cases(data3)
-        data4 <- data3[good,]
-
+        data3[data3[,2] == "Not Available",] <- NA
+        data4 <- na.omit(data3)
+        data4[,2] <- as.numeric(data4[,2])
+        
+        data5 <- data4[order(data4[,2], data4[,1]),]
+        
         if(num == "best"){
-                return(data4[1,1])
-        }
-        if(num == "worst"){
-                return(data4[nrow(data4),1])
-        }
-        if(num %in% 1:nrow(data4)){
-                return(data4[num,1])
-        } else {
+                return(data5[1,1])
+        } else if(num == "worst"){
+                return(data5[nrow(data5),1])
+        } else if(num %in% 1:nrow(data5)){
+                return(data5[num,1])
+        } else{
                 return(NA)
         }
 }
